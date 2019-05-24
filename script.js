d6246e59b0f188c84018c3f78f08b91aaef9b242
Lagrek <43035380+Lagrek@users.noreply.github.com>
2019-05-24 13:54:04 +0300

    NewOrder

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-
-	$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-var ele = document.getElementById('menu-min');
-
-
-
-if ($(window).width() < 700) {
-	var element = document.getElementById('menu');
-	element.remove();
-	$(ele).css('display', 'block');
-} else {
-	console.log('false');
-}
-
 var slideEl = $(".slide--parent");
 
 slideEl.flickity({
-	imagesLoaded: true,
+	imagesLoaded: false,
 	wrapAround: true,
-	autoPlay: 8000,
+	autoPlay: 2500,
 	prevNextButtons:false,
 });
 
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
